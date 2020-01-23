b3114666de8189b13ffb666604f69ddcd73bcf9b
garciajesv <garciajesv@globalhitss.com>
2020-01-23 12:43:17 -0600

    codigos de produccion

---
+++ Carpeta de codigo/Metodos.java
@@ -0,0 +1,8 @@
+
+public class Metodos{
+
+public String Saludo(String nom){
+return "Hola que pex: "+nom;
+}
+
+}
---
+++ Carpeta de codigo/hola.java
@@ -0,0 +1,7 @@
+
+public class hola{
+public static void main(String [] args){
+Metodos met = new Metodos();
+System.out.println(met.Saludo("Yoni-Laboriel"));
+}
+}
