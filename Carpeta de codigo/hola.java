b4811a985023ab9e5f2b3461860fb100fbd2c1cb
garciajesv <garciajesv@globalhitss.com>
2020-01-23 12:52:14 -0600

    cambio mio de mi

--- Carpeta de codigo/hola.java
+++ Carpeta de codigo/hola.java
@@ -3,5 +3,6 @@ public class hola{
 public static void main(String [] args){
 Metodos met = new Metodos();
 System.out.println(met.Saludo("Yoni-Laboriel"));
+System.out.println("CAMBIO MIO DE MI");
 }
 }
