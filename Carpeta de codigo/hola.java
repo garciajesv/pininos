0db33099c1f0e877f04bc593c99ec52ccede338c
SeijuroHiko13vo <kuzuryusen_vivi@hotmail.com>
2020-01-23 13:59:54 -0600

    Cambio de Seijuro

--- Carpeta de codigo/hola.java
+++ Carpeta de codigo/hola.java
@@ -4,5 +4,6 @@ public static void main(String [] args){
 Metodos met = new Metodos();
 System.out.println(met.Saludo("Yoni-Laboriel"));
 System.out.println("CAMBIO MIO DE MI");
+System.out.println("CAMBIO DE SEIJURO");
 }
 }
